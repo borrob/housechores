8e5f1768ed119ebc4c7df0579e10031264f5b0bf
Rob van Loon <borrob@me.com>
2015-08-16 10:10:43 +0000

    Added Anne as user and added some more chores

--- sql/insert_sampledata.sql
+++ sql/insert_sampledata.sql
@@ -1,6 +1,7 @@
 insert into persons values (-1,'none','',-1);
 insert into persons values (2,'rob','rob',1);
-insert into persons values (3,'random','asd',2);
+insert into persons values (3,'anne','anne',1);
+insert into persons values (4,'random','asd',2);
 
 insert into chores values (-1,'none');
 insert into chores values (1,'dishes');
@@ -8,6 +9,10 @@ insert into chores values (2,'vacuum');
 insert into chores values (3,'groceries jumbo');
 insert into chores values (4,'groceries lidl');
 insert into chores values (5,'change bedsheets');
+insert into chores values (6,'clean toilet');
+insert into chores values (7,'clean bathroom');
+insert into chores values (8,'clean cat litter');
+insert into chores values (9,'change cat litter');
 
 insert into actions values (1,'2015-08-01',1,1);
 insert into actions values (2,'2015-08-02',1,2);
@@ -15,3 +20,4 @@ insert into actions values (3,'2015-08-01',1,3);
 insert into actions values (4,'2015-08-02',1,2);
 insert into actions values (5,'2015-08-01',1,5);
 insert into actions values (6,'2015-08-02',1,1);
+insert into actions values (7,'2015-08-16',3,6);
